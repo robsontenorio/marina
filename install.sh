2bc882fd8b8277a1bd778b070c4948128476bd62
Robson Tenório <rrtenorio@gmail.com>
2024-03-29 16:43:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,19 +7,22 @@ read -p 'Github username: ' ghusername
 read -p 'Github token: ' ghtoken
 
 # docker
-curl -fsSL https://get.docker.com -o get-docker.sh &&
-sudo sh ./get-docker.sh &&
-sudo systemctl enable docker.service &&
-sudo systemctl enable containerd.service &&
-
-# Github Registry
-export CR_PAT=$ghtoken &&
-echo $CR_PAT| docker login ghcr.io -u $ghusername --password-stdin &&
+if ! docker --version; then
+  curl -fsSL https://get.docker.com -o get-docker.sh &&
+  sudo sh ./get-docker.sh &&
+  sudo systemctl enable docker.service &&
+  sudo systemctl enable containerd.service &&
+fi
 
 # https://github.com/Wowu/docker-rollout
 mkdir -p ~/.docker/cli-plugins &&
 curl https://raw.githubusercontent.com/wowu/docker-rollout/master/docker-rollout -o ~/.docker/cli-plugins/docker-rollout &&
 chmod +x ~/.docker/cli-plugins/docker-rollout &&
+
+# Github Registry
+export CR_PAT=$ghtoken &&
+echo $CR_PAT| docker login ghcr.io -u $ghusername --password-stdin &&
+
 
 # Create Project
 mkdir -p ~/projects/$folder &&
