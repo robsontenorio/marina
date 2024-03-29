a9ffab493ae8c772e546de098c80249fa661b02b
Robson Tenório <rrtenorio@gmail.com>
2024-03-29 16:39:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,28 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
 clear
 
-read 'Folder name (ex: myapp): ' folder
-read 'Github username: ' ghusername
-read 'Github token: ' ghtoken
+read -p 'Folder name (ex: myapp): ' folder
+read -p 'Github username: ' ghusername
+read -p 'Github token: ' ghtoken
 
 # docker
-#curl -fsSL https://get.docker.com -o get-docker.sh &&
-#sudo sh ./get-docker.sh &&
-#sudo systemctl enable docker.service && 
-#sudo systemctl enable containerd.service && 
+curl -fsSL https://get.docker.com -o get-docker.sh &&
+sudo sh ./get-docker.sh &&
+sudo systemctl enable docker.service &&
+sudo systemctl enable containerd.service &&
 
 # Github Registry
 export CR_PAT=$ghtoken &&
-echo $CR_PAT| docker login ghcr.io -u $ghusername --password-stdin && 
+echo $CR_PAT| docker login ghcr.io -u $ghusername --password-stdin &&
 
 # https://github.com/Wowu/docker-rollout
-#mkdir -p ~/.docker/cli-plugins &&
-#curl https://raw.githubusercontent.com/wowu/docker-rollout/master/docker-rollout -o ~/.docker/cli-plugins/docker-rollout && 
-#chmod +x ~/.docker/cli-plugins/docker-rollout &&
+mkdir -p ~/.docker/cli-plugins &&
+curl https://raw.githubusercontent.com/wowu/docker-rollout/master/docker-rollout -o ~/.docker/cli-plugins/docker-rollout &&
+chmod +x ~/.docker/cli-plugins/docker-rollout &&
 
-# Project Ping17
+# Create Project
 mkdir -p ~/projects/$folder &&
-cd ~/projects/$folder && 
-touch .env.docker && 
+cd ~/projects/$folder &&
+touch .env.docker &&
 touch docker-compose.yml
