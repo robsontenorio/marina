fbc6b682afee517ea2809a37da6e5d64454d4661
Robson Tenório <rrtenorio@gmail.com>
2024-04-14 12:47:16 -0300

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // TODO: pass env variable on docker run to create it ?
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make(2222)
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,29 @@ use App\Jobs\FetchDockerStatsJob;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'dashboard');
-Volt::route('/all-stacks', 'stacks.index');
-Volt::route('/stacks/{hash}/{stack}/edit', 'stacks.edit');
-Volt::route('/stacks/create', 'stacks.create');
-Volt::route('/stacks/{hash}/{stack}', 'stacks.show');
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
 
-// Put `docker stats` in cache for a few seconds
-Route::get('/stats', function () {
-    FetchDockerStatsJob::dispatchAfterResponse();
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/');
 });
+
+Route::middleware('auth')->group(function () {
+    Volt::route('/', 'dashboard');
+    Volt::route('/all-stacks', 'stacks.index');
+    Volt::route('/stacks/{hash}/{stack}/edit', 'stacks.edit');
+    Volt::route('/stacks/create', 'stacks.create');
+    Volt::route('/stacks/{hash}/{stack}', 'stacks.show');
+
+    // Put `docker stats` in cache for a few seconds
+    Route::get('/stats', function () {
+        FetchDockerStatsJob::dispatchAfterResponse();
+    });
+});
+
+
