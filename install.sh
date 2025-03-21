46c950b297b75aa034b2efefb0d449e73a279cc3
Robson Tenório <rrtenorio@gmail.com>
2025-03-21 17:46:10 -0300

    WIP

--- install.sh
+++ install.sh
@@ -2,32 +2,34 @@
 set -e
 
 # Install Docker
-echo -e "\n\n---\033[1;32mInstalling Docker\n---\033[0m"
+echo "\033[1;32m\n\n---\nInstalling Docker ...\n---\033[0m"
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
 # Enable Docker services
-echo -e "\n\n---\033[1;32mEneable Docker at system startup\n---\033[0m"
+echo "\033[1;32m\n\n---\nEneabling Docker at system startup...\n---\033[0m"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-echo -e "\n\n---\033[1;32mEnable Docker Wwarm mode\n---\033[0m"
+echo "\033[1;32m\n\n---\nEneabling Docker Swarm mode ...\n---\033[0m"
 # Initialize Docker Swarm
-if ! docker info | grep -q "Swarm: active"; then
-    docker swarm init
-fi
+docker swarm init || true
 
 # Create a common network
-echo -e "\n\n---\033[1;32mCreate 'marina' network\n---\033[0m"
-docker network create --driver overlay marina
+echo "\033[1;32m\n\n---\nCreateting 'marina' network ...\n---\033[0m"
+docker network create --driver overlay marina || true
 
 # Create Docker volume and network
-echo -e "\n\n---\033[1;32mCreate 'marina_data' volume\n---\033[0m"
+echo "\033[1;32m\n\n---\nCreateting 'marina_data' volume ...\n---\033[0m"
 docker volume create marina_data
 
+# Remove Marina if it exists
+echo "\033[1;32m\n\n---\nStopping 'marina' service ...\n---\033[0m"
+docker service rm marina || true
+
 # Create Marina service
-echo -e "\n\n---\033[1;32mStarting 'marina'service ...\n---\033[0m"
+echo "\033[1;32m\n\n---\nStarting 'marina'service ...\n---\033[0m"
 docker service create \
     --name marina \
     --network marina \
@@ -36,4 +38,4 @@ docker service create \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     ghcr.io/robsontenorio/marina:production
 
-echo -e "\n\n\033[1;32mDocker installed. Swarm mode active. Marina is up at http://SERVER-IP:8787\033[0m"
+echo "\n\n\033[1;32mDocker installed. Swarm mode active. Marina is up at http://SERVER-IP:8787\033[0m"
