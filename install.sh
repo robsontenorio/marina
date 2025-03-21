43f7885f0c974e11cebf002b452942380d1c6edd
Robson Tenório <rrtenorio@gmail.com>
2025-03-21 17:36:14 -0300

    WIP

--- install.sh
+++ install.sh
@@ -2,23 +2,32 @@
 set -e
 
 # Install Docker
+echo -e "\n\n---\033[1;32mInstalling Docker\n---\033[0m"
+
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
 # Enable Docker services
+echo -e "\n\n---\033[1;32mEneable Docker at system startup\n---\033[0m"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
+echo -e "\n\n---\033[1;32mEnable Docker Wwarm mode\n---\033[0m"
 # Initialize Docker Swarm
-docker swarm init || true
+if ! docker info | grep -q "Swarm: active"; then
+    docker swarm init
+fi
 
 # Create a common network
+echo -e "\n\n---\033[1;32mCreate 'marina' network\n---\033[0m"
 docker network create --driver overlay marina
 
 # Create Docker volume and network
+echo -e "\n\n---\033[1;32mCreate 'marina_data' volume\n---\033[0m"
 docker volume create marina_data
 
 # Create Marina service
+echo -e "\n\n---\033[1;32mStarting 'marina'service ...\n---\033[0m"
 docker service create \
     --name marina \
     --network marina \
@@ -27,4 +36,4 @@ docker service create \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     ghcr.io/robsontenorio/marina:production
 
-echo -e "\n\n\033[1;32mDocker installed. Swarm mode active. Marina is up. Hit http://SERVER-IP:8787\033[0m"
+echo -e "\n\n\033[1;32mDocker installed. Swarm mode active. Marina is up at http://SERVER-IP:8787\033[0m"
