e52bf3d7ccd4734d091ca114b55dd86a6575d32f
Robson Tenório <rrtenorio@gmail.com>
2025-03-21 18:44:11 -0300

    WIP

--- install.sh
+++ install.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 set -e
 
+# Check if Docker is installed
+command -v docker >/dev/null && echo -e "\n\e[91m 🚫 Error: Docker is already installed. You can not run this script. \e[0m\n" && exit 1
+
 # Install Docker
-echo "\033[1;32m\n\n---\nInstalling Docker ...\n---\033[0m"
+echo "\033[96m\n\n\n✨ Installing Docker ...\n\033[0m"
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 
 # Enable Docker services
-echo "\033[1;32m\n\n---\nEneabling Docker at system startup...\n---\033[0m"
+echo "\033[96m\n\n\n✨ Eneabling Docker at system startup...\n\033[0m"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-echo "\033[1;32m\n\n---\nEneabling Docker Swarm mode ...\n---\033[0m"
+echo "\033[96m\n\n\n✨ Eneabling Docker Swarm mode ...\n\033[0m"
 # Initialize Docker Swarm
-docker swarm init || true
+docker swarm init 
 
 # Create a common network
-echo "\033[1;32m\n\n---\nCreateting 'marina' network ...\n---\033[0m"
-docker network create --driver overlay marina || true
+echo "\033[96m\n\n\n✨ Creating 'marina' network ...\n\033[0m"
+docker network create --driver overlay marina 
 
 # Create Docker volume and network
-echo "\033[1;32m\n\n---\nCreateting 'marina_data' volume ...\n---\033[0m"
+echo "\033[96m\n\n\n✨ Creating 'marina_data' volume ...\n\033[0m"
 docker volume create marina_data
 
-# Remove Marina if it exists
-echo "\033[1;32m\n\n---\nStopping 'marina' service ...\n---\033[0m"
-docker service rm marina || true
-
 # Create Marina service
-echo "\033[1;32m\n\n---\nStarting 'marina'service ...\n---\033[0m"
+echo "\033[96m\n\n\n✨ Starting 'marina'service ...\n\033[0m"
+
 docker service create \
     --name marina \
     --network marina \
@@ -38,4 +38,5 @@ docker service create \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     ghcr.io/robsontenorio/marina:production
 
-echo "\n\n\033[1;32mDocker installed. Swarm mode active. Marina is up at http://SERVER-IP:8787\033[0m"
+echo "\n\n\033[1;32m\n✅ Docker installed and swarm mode is active.\033[0m"
+echo "\n\033[1;32m\n🚀 Marina is up at http://your-server-ip:8787\n\n\033[0m"
