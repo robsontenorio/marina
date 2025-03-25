6bea6e2695010538ddae9dff8b01d253c71ae7a2
Robson Tenório <rrtenorio@gmail.com>
2025-03-25 17:24:41 -0300

    WIP

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -2,11 +2,15 @@
 
 echo '------ Starting deploy tasks  ------'
 
+RUN composer install --prefer-dist --no-interaction --no-progress --ansi
+RUN cp .env.example .env
+RUN php artisan key:generate
+
 RUN yarn install && yarn build
 
 touch /var/www/app/.data/database.sqlite
-php artisan migrate --seed --force
 
+php artisan migrate --seed --force
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
