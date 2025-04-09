11d85fdbccb8b4831cfca8ca9bf89f8e5adcb633
Robson Tenório <rrtenorio@gmail.com>
2025-04-08 22:00:28 -0300

    WIP

--- install.sh
+++ install.sh
@@ -26,6 +26,13 @@ docker network create --driver overlay marina || true
 echo "\033[96m\n\n\n✨ Creating 'marina' volume ...\n\033[0m"
 docker volume create marina || true
 
+# Get the current version
+TAG=$(curl -s https://api.github.com/repos/robsontenorio/marina/tags \
+  | jq -r '.[].name' \
+  | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' \
+  | sort -Vr \
+  | head -n1)
+
 # Create Marina service
 echo "\033[96m\n\n\n✨ Starting 'marina'service ...\n\033[0m"
 docker service create \
@@ -36,7 +43,7 @@ docker service create \
     --mount type=volume,source=marina,target=/app/.data \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock,ro \
     --group $(stat -c '%g' /var/run/docker.sock) \
-    ghcr.io/robsontenorio/marina
+    ghcr.io/robsontenorio/marina:$TAG
 
 echo "\n\n\033[1;32m\n✅ Docker installed and swarm mode is active.\033[0m"
 echo "\n\033[1;32m\n🚀 Marina is up at http://your-server-ip:8787\n\n\033[0m"
